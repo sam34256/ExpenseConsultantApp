445a96beff39e71b7dc580c7eb860ad8a4619dcb
sam34256 <samueltamiru11b@gmail.com>
2023-02-13 17:22:21 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.expenseconsultantapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.expenseconsultantapp to javafx.fxml;
+    exports com.example.expenseconsultantapp;
+}
