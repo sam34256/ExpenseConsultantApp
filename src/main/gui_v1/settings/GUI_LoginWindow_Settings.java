0ae5e93c1bb6af3ed4dd9bb33c5ffb0214844056
yefran2015 <yefran2015@gmail.com>
2023-02-18 23:38:00 -0600

    settings for login window

---
+++ src/main/gui_v1/settings/GUI_LoginWindow_Settings.java
@@ -0,0 +1,12 @@
+package gui_v1.settings;
+
+import java.awt.*;
+
+public interface GUI_LoginWindow_Settings {
+    String strLogInWindowTitle = "Log in";
+    String strLogInHeadTitle = "Please Enter Your Log In Info";
+    String strCopyRigts = "Copyright \u00a9 SPAM Team 2023";
+    int gui_width = 400;
+    int gui_height =200;
+    Dimension logInFFrameSize = new Dimension(gui_width, gui_height);
+}
